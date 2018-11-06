4513d19c636c7be49ea9c570ba3027b798209e05
RebGov <becci.govert@gmail.com>
2018-11-05 20:18:00 -0600

    logout button added to page

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,35 +1,10 @@
 
-// export function getUsernameValue(e){
-//   return ({
-//     type: 'GET_USERNAME_VALUE',
-//     payload: e.target.value
-//   })
-// }
-// export function getEmailValue(e){
-//   return ({
-//     type: 'GET_EMAIL_VALUE',
-//     payload: e.target.value
-//   })
-// }
-// export function getPasswordValue(e){
-//   return ({
-//     type: 'GET_PASSWORD_VALUE',
-//     payload: e.target.value
-//   })
-// }
-// export function getFirstNameValue(e){
-//   return ({
-//     type: 'GET_FIRST_NAME_VALUE',
-//     payload: e.target.value
-//   })
-// }
-// export function getLastNameValue(e){
-//   return ({
-//     type: 'GET_LAST_NAME_VALUE',
-//     payload: e.target.value
-//   })
-// }
-
+export function updateNewUser(userAttributes){
+  return ({
+    type: 'UPDATE_NEW_USER',
+    payload: userAttributes
+  })
+}
 export function updateCurrentUser(userAttributes){
   return ({
     type: 'UPDATE_CURRENT_USER',
@@ -41,32 +16,36 @@ export function loginUser(){
     type: 'ATTEMPT_TO_LOGIN_USER'
   })
 }
+
+
+
 export function createUser(){
   return ({
     type: 'CREATE_NEW_USER'
   })
 }
 
-  //seperate out each item being added
-  //username, email, password etc.?
 
 export function getUserData(jwt){
+  return dispatch => {
+    fetch('http://localhost:3000/api/v1/profile', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${jwt}`
+      }
+    })
+    .then(resp => resp.json())
+    .then(payload =>
+      // if(!payload.error){
+        dispatch( { type: 'GET_USER_GAME_DATA', payload: payload } )
+      // }
+    )
+  }
+}
 
-  // debugger
-    return dispatch => {
-      fetch('http://localhost:3000/api/v1/profile', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${jwt}`
-        }
-      })
-      .then(resp => resp.json())
-      .then(payload =>
-        // if(!payload.error){
-          dispatch( { type: 'GET_USER_GAME_DATA', payload: payload } )
-        // }
-      )
-    }
-
+export function logoutUser(){
+  return ({
+    type: 'LOGOUT_USER'
+  })
 }
